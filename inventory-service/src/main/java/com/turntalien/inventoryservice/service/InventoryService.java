74d079464562ff79d9b64274e9df143b69010087
turntalienn <aju369420@gmail.com>
2022-07-13 07:11:48 +0530

    implemented circuit breakers

--- inventory-service/src/main/java/com/turntalien/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/turntalien/inventoryservice/service/InventoryService.java
@@ -3,6 +3,8 @@ package com.turntalien.inventoryservice.service;
 import com.turntalien.inventoryservice.dto.InventoryResponse;
 import com.turntalien.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,12 +12,17 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryService {
 
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
+    @SneakyThrows //unsafe for production
     public List<InventoryResponse> isInStock(List<String> skuCode) {
+        log.info("wait started");
+        Thread.sleep(10000);
+        log.info("wait ended");
         return inventoryRepository.findBySkuCodeIn(skuCode).stream()
                 .map(inventory ->
                     InventoryResponse.builder()
